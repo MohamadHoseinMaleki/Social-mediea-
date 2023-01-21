526cd06db7423af8a9ebbc06a0cb458e20a63d14
mohamad <mhmm69923@gmail.com>
2023-01-21 23:25:31 +0330

    change wirtFile.java

--- src/File/WritFile.java
+++ src/File/WritFile.java
@@ -20,19 +20,9 @@ public class WritFile {
         JSONObject employeeObject = new JSONObject();
         employeeObject.put("employee", employeeDetails);
 
-        //Second Employee
-        JSONObject employeeDetails2 = new JSONObject();
-        employeeDetails2.put("firstName", "Brian");
-        employeeDetails2.put("lastName", "Schultz");
-        employeeDetails2.put("website", "example.com");
-
-        JSONObject employeeObject2 = new JSONObject();
-        employeeObject2.put("employee", employeeDetails2);
-
         //Add employees to list
         JSONArray employeeList = new JSONArray();
         employeeList.add(employeeObject);
-        employeeList.add(employeeObject2);
 
         //Write JSON file
         try (FileWriter file = new FileWriter("employees.json")) {
